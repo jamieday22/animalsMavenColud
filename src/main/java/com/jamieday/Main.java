062d249cf3420a4473879accd73a359bdf19c2b5
jamieday22 <jamierobertday@hotmail.com>
2022-01-25 12:14:57 +0000

    third test done

--- src/main/java/com/jamieday/Main.java
+++ src/main/java/com/jamieday/Main.java
@@ -5,11 +5,5 @@ public class Main {
         public static void main(String[] args){
 
 
-            Cat Cat = new Cat();
-            Cat.setWeight(22);
-            Cat.setHeight(22);
-            System.out.println("the height of the cat is "+Cat.getWeight());
-            System.out.println("the weight of the cat is "+Cat.getHeight());
-
         }
     }
